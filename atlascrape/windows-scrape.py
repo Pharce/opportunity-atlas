7b713b52fd72f01a6af4226f2f9348c2c8139573
Pharce <Pharce@github.com>
2020-12-09 12:26:05 -0800

    windows-scrape goes from tract-id to screenshot
    Server runs in windows using MSEDGE. OS system must be windows in this installation. Otherwise switch to headless driver on linux.

--- atlascrape/windows-scrape.py
+++ atlascrape/windows-scrape.py
@@ -1,10 +1,13 @@
 import time
+import io
 from msedge.selenium_tools import Edge, EdgeOptions
+from selenium.webdriver.common.keys import Keys
+
 
 # Launch Microsoft Edge (EdgeHTML)
 if __name__ == '__main__':
 
-    tract_id = ''
+    tract_id = '38077970700'
 
     driver = Edge(executable_path=r"C:\Users\faris\documents\akeb\opportunity-atlas\atlascrape\msedgedriver.exe")
 
@@ -13,8 +16,12 @@ if __name__ == '__main__':
     options.use_chromium = True
     driver = Edge(options = options)
 
+    print("Getting atlas")
     # get opportunity atlas
     driver.get("https://opportunityatlas.org/")
+
+    time.sleep(10)
+
 
     # click on get started atlas
     get_started = driver.find_element_by_xpath("/html/body/div[3]/div[2]/div[1]/p[5]/button")
@@ -35,6 +42,8 @@ if __name__ == '__main__':
     search_box.send_keys(Keys.ENTER)
     time.sleep(10)
 
-    with io.BytesIO(driver.get_screenshot_as_png()) as f:
-        f.write('./test.png')
+    driver.save_screenshot('test.png')
+    
+    #with io.BytesIO(driver.get_screenshot_as_png()) as f:
+    #    f.write('./test.png')
     driver.close()
